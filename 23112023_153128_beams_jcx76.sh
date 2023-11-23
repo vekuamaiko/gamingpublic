5895535724b329b4a2fc6ccf5deefdf8d699ca6a
vekuamaiko <vekuamaiko0@gmail.com>
2023-11-23 15:31:28 +0000

    Updating beams

---
+++ 23112023_153128_beams_jcx76.sh
@@ -0,0 +1,1 @@
+There are mirrors on the moon. Astronauts left them so that laser beams could be bounced off of them from Earth. These beams help give us the distance to the moon give or take a few metres.
